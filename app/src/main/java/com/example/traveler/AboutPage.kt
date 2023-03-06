38a092b0994035e276d36d0dea7c8947dfe1a762
Rayhan Illyas <rayhan205732@gmail.com>
2023-03-07 06:03:31 +0700

    finish about page

--- app/src/main/java/com/example/traveler/AboutPage.kt
+++ app/src/main/java/com/example/traveler/AboutPage.kt
@@ -2,10 +2,26 @@ package com.example.traveler
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.traveler.databinding.ActivityAboutPageBinding
 
 class AboutPage : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAboutPageBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_about_page)
+        binding = ActivityAboutPageBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.btnAboutBack.setOnClickListener {
+            finish()
+        }
+
+        binding.btnFollow.setOnClickListener {
+            val toast = Toast.makeText(this, "Followed", Toast.LENGTH_SHORT)
+            toast.show()
+        }
     }
 }
